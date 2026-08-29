afd4fa1c431f212d55c57d756062ae640555b674
zilzal <zlzalh810z@gmail.com>
2026-08-30 00:20:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ضع بيانات مشروع Supabase هنا.
 // استخدم Publishable key (أو anon key في المشاريع القديمة) فقط.
 // لا تضع service_role key داخل هذا الملف.
-window.SUPABASE_URL = "ضع_رابط_مشروعك_هنا";
-window.SUPABASE_KEY = "ضع_PUBLISHABLE_KEY_هنا";
+window.SUPABASE_URL = "https://enzqxewgnfgpvjcsitsu.supabase.co";
+window.SUPABASE_KEY = "sb_publishable_H0QnFr9gC3rwe6X0a662Tg_ISX3MdHY";
