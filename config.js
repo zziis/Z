ccfaf88fa13a17b976653c4c6542341f227c1142
zilzal <zlzalh810z@gmail.com>
2026-08-30 15:48:50 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// ضع بيانات مشروع Supabase هنا.
-// استخدم Publishable key (أو anon key في المشاريع القديمة) فقط.
-// لا تضع service_role key داخل هذا الملف.
-window.SUPABASE_URL = "https://enzqxewgnfgpvjcsitsu.supabase.co";
-window.SUPABASE_KEY = "sb_publishable_H0QnFr9gC3rwe6X0a662Tg_ISX3MdHY";
