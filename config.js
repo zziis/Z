126917a193fe3916161ad524539699023f4f4f87
zilzal <zlzalh810z@gmail.com>
2026-08-30 15:50:26 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ضع بيانات مشروع Supabase هنا.
+// استخدم Publishable key (أو anon key في المشاريع القديمة) فقط.
+// لا تضع service_role key داخل هذا الملف.
+window.SUPABASE_URL = "ضع_رابط_مشروعك_هنا";
+window.SUPABASE_KEY = "ضع_PUBLISHABLE_KEY_هنا";
